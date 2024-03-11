0da805506dd0b0113b0af6fdace20eaa1d078d11
Sangmin Hwang <hwsmin920@gmail.com>
2024-03-11 18:23:55 +0900

    feat: redesign all prompts and delete planning prompt

--- assignment2/prompt.py
+++ assignment2/prompt.py
@@ -1,11 +1,4 @@
-SYSTEM_PROMPT = """You are an agent designed to develop a web app from a user prompt. If you use tools, use a tool once at a time."""
-
-
-PLAN_PROMPT = """{task}
-
-At first, write a concise plan for the implementation.
-Respond ONLY with a list of steps. Each step should be written in one line.
-"""
+SYSTEM_PROMPT = """You are an agent designed to develop a web app from a user prompt. You can use tools. If you use tools, use a tool once at a time."""
 
 
 REASONING_PROMPT = """Implement a web app from a user prompt.
@@ -14,12 +7,12 @@ User prompt: {prompt}
 Here is a history of what you've done so far:
 {history}
 
-What is the next step to do? Answer with a single line of text. Don't miss any details needed for implementation.
-If you think you finished the task, respond with a single text 'Done'.
+What is the next step to do? Answer with a single line of natural langauge text. Don't miss any details needed for implementation.
+If you think you finished the task, respond with a single text 'DONE: <terminal command to run the app>'.
 """
 
 
-ACT_PROMPT = """Implement a web app from a user prompt.
+ACT_PROMPT = """Implement a web app from a user prompt. Refer to the history and do the following TODO.
 
 User prompt: {prompt}
 
@@ -30,7 +23,7 @@ Here is a history of what you've done so far:
 What to do next: {thought}
 """
 
-OBSERVATION_FORMAT = """Thought: {thought}
+HISTORY_FORMAT = """Thought: {thought}
 Action: {action}
 Observation: {observation}
 --------------------------------
